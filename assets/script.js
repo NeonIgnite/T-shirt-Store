5e8ae6537cc1d60ea718025f6923b528d56a911e
NeonIgnite <khaledashraf310@gmail.com>
2023-04-29 14:50:50 +0300

    done

---
+++ assets/script.js
@@ -0,0 +1,289 @@
+// let img=document.getElementById('image')
+// img.addEventListener('mouseover',function(){
+//     img.setAttribute('src','assets\Images\product-06-a.jpg');
+//     // image.style.width = `200px`;
+// })
+// img.addEventListener('mouseout',function(){
+//     img.setAttribute('src','assets\Images\product-05-a.jpg');
+// })
+
+// // function RollOver(){
+// //     document.image.style.backgroundImage = `url('E:\AMIT Course\shirtFactory\assets\Images\product-06-a.jpg')`
+// // }
+let imageContent1=document.getElementById('imageContent1')
+let hoverContent1=document.getElementById('hoverContent1')
+let hoverCart1=document.getElementById('hoverCart1')
+let hoverEye1=document.getElementById('hoverEye1')
+let image1=document.getElementById('image1')
+
+let imageContent2=document.getElementById('imageContent2')
+let hoverContent2=document.getElementById('hoverContent2')
+let hoverCart2=document.getElementById('hoverCart2')
+let hoverEye2=document.getElementById('hoverEye2')
+let image2=document.getElementById('image2')
+
+let imageContent3=document.getElementById('imageContent3')
+let hoverContent3=document.getElementById('hoverContent3')
+let hoverCart3=document.getElementById('hoverCart3')
+let hoverEye3=document.getElementById('hoverEye3')
+let image3=document.getElementById('image3')
+
+let imageContent4=document.getElementById('imageContent4')
+let hoverContent4=document.getElementById('hoverContent4')
+let hoverCart4=document.getElementById('hoverCart4')
+let hoverEye4=document.getElementById('hoverEye4')
+let image4=document.getElementById('image4')
+
+let imageContent5=document.getElementById('imageContent5')
+let hoverContent5=document.getElementById('hoverContent5')
+let hoverCart5=document.getElementById('hoverCart5')
+let hoverEye5=document.getElementById('hoverEye5')
+let image5=document.getElementById('image5')
+let sale1=document.getElementById('sale1')
+
+let imageContent6=document.getElementById('imageContent6')
+let hoverContent6=document.getElementById('hoverContent6')
+let hoverCart6=document.getElementById('hoverCart6')
+let hoverEye6=document.getElementById('hoverEye6')
+let image6=document.getElementById('image6')
+let sale2=document.getElementById('sale2')
+
+let imageContent7=document.getElementById('imageContent7')
+let hoverContent7=document.getElementById('hoverContent7')
+let hoverCart7=document.getElementById('hoverCart7')
+let hoverEye7=document.getElementById('hoverEye7')
+let image7=document.getElementById('image7')
+let sale3=document.getElementById('sale3')
+
+let imageContent8=document.getElementById('imageContent8')
+let hoverContent8=document.getElementById('hoverContent8')
+let hoverCart8=document.getElementById('hoverCart8')
+let hoverEye8=document.getElementById('hoverEye8')
+let image8=document.getElementById('image8')
+let sale4=document.getElementById('sale4')
+
+imageContent1.addEventListener('mouseover',function(){
+    hoverContent1.style.visibility='visible'
+});
+imageContent1.addEventListener('mouseout',function(){
+    hoverContent1.style.visibility='hidden'
+});
+hoverEye1.addEventListener('mouseover', function(){
+    image1.src='assets/Images/product-01-c.jpg';
+    hoverEye1.style.color='black';
+});
+hoverEye1.addEventListener('mouseout', function(){
+    image1.src='assets/Images/product-01-c.jpg';
+    hoverEye1.style.color='gray';
+});
+hoverCart1.addEventListener('mouseover', function(){
+    image1.src='assets/Images/product-01-c.jpg'
+    hoverCart1.style.color='black'
+});
+hoverCart1.addEventListener('mouseout', function(){
+    image1.src='assets/Images/product-01-c.jpg'
+    hoverCart1.style.color='gray'
+
+});
+
+// **********
+imageContent2.addEventListener('mouseover',function(){
+    hoverContent2.style.visibility='visible'
+});
+imageContent2.addEventListener('mouseout',function(){
+    hoverContent2.style.visibility='hidden'
+});
+hoverEye2.addEventListener('mouseover', function(){
+    image2.src='assets/Images/product-01-d.jpg';
+    hoverEye2.style.color='black';
+});
+hoverEye2.addEventListener('mouseout', function(){
+    image2.src='assets/Images/product-01-d.jpg';
+    hoverEye2.style.color='gray';
+});
+hoverCart2.addEventListener('mouseover', function(){
+    image2.src='assets/Images/product-01-d.jpg'
+    hoverCart2.style.color='black'
+});
+hoverCart2.addEventListener('mouseout', function(){
+    image2.src='assets/Images/product-01-d.jpg'
+    hoverCart2.style.color='gray'
+
+});
+// // ***********
+
+imageContent3.addEventListener('mouseover',function(){
+    hoverContent3.style.visibility='visible'
+});
+imageContent3.addEventListener('mouseout',function(){
+    hoverContent3.style.visibility='hidden'
+});
+hoverEye3.addEventListener('mouseover', function(){
+    image3.src='assets/Images/product-001-b.jpg';
+    hoverEye3.style.color='black';
+});
+hoverEye3.addEventListener('mouseout', function(){
+    image3.src='assets/Images/product-001-b.jpg';
+    hoverEye3.style.color='gray';
+});
+hoverCart3.addEventListener('mouseover', function(){
+    image3.src='assets/Images/product-001-b.jpg'
+    hoverCart3.style.color='black'
+});
+hoverCart3.addEventListener('mouseout', function(){
+    image3.src='assets/Images/product-001-b.jpg'
+    hoverCart3.style.color='gray'
+
+});
+// **********
+imageContent4.addEventListener('mouseover',function(){
+    hoverContent4.style.visibility='visible'
+});
+imageContent4.addEventListener('mouseout',function(){
+    hoverContent4.style.visibility='hidden'
+});
+hoverEye4.addEventListener('mouseover', function(){
+    image4.src='assets/Images/product-01-d.jpg';
+    hoverEye4.style.color='black';
+});
+hoverEye4.addEventListener('mouseout', function(){
+    image4.src='assets/Images/product-01-d.jpg';
+    hoverEye4.style.color='gray';
+});
+hoverCart4.addEventListener('mouseover', function(){
+    image4.src='assets/Images/product-01-d.jpg'
+    hoverCart4.style.color='black'
+});
+hoverCart4.addEventListener('mouseout', function(){
+    image4.src='assets/Images/product-01-d.jpg'
+    hoverCart4.style.color='gray'
+
+});
+// *************another Section****************
+imageContent5.addEventListener('mouseover',function(){
+    hoverContent5.style.visibility='visible'
+});
+imageContent5.addEventListener('mouseout',function(){
+    hoverContent5.style.visibility='hidden'
+});
+hoverEye5.addEventListener('mouseover', function(){
+    image5.src='assets/Images/product-01-c.jpg';
+    hoverEye5.style.color='black';
+});
+hoverEye5.addEventListener('mouseout', function(){
+    image5.src='assets/Images/product-01-c.jpg';
+    hoverEye5.style.color='gray';
+});
+hoverCart5.addEventListener('mouseover', function(){
+    image5.src='assets/Images/product-01-c.jpg'
+    hoverCart5.style.color='black'
+});
+hoverCart5.addEventListener('mouseout', function(){
+    image5.src='assets/Images/product-01-c.jpg'
+    hoverCart5.style.color='gray'
+
+});
+
+
+
+imageContent6.addEventListener('mouseover',function(){
+    hoverContent6.style.visibility='visible'
+});
+imageContent6.addEventListener('mouseout',function(){
+    hoverContent6.style.visibility='hidden'
+});
+hoverEye6.addEventListener('mouseover', function(){
+    image6.src='assets/Images/product-01-d.jpg';
+    hoverEye6.style.color='black';
+});
+hoverEye6.addEventListener('mouseout', function(){
+    image6.src='assets/Images/product-01-d.jpg';
+    hoverEye6.style.color='gray';
+});
+hoverCart6.addEventListener('mouseover', function(){
+    image6.src='assets/Images/product-01-d.jpg';
+    hoverCart6.style.color='black'
+});
+hoverCart6.addEventListener('mouseout', function(){
+    image6.src='assets/Images/product-01-d.jpg';
+    hoverCart6.style.color='gray'
+
+});
+
+
+imageContent7.addEventListener('mouseover',function(){
+    hoverContent7.style.visibility='visible'
+});
+imageContent7.addEventListener('mouseout',function(){
+    hoverContent7.style.visibility='hidden'
+});
+hoverEye7.addEventListener('mouseover', function(){
+    image7.src='assets/Images/product-001-b.jpg';
+    hoverEye7.style.color='black';
+});
+hoverEye7.addEventListener('mouseout', function(){
+    image7.src='assets/Images/product-001-b.jpg';
+    hoverEye7.style.color='gray';
+});
+hoverCart7.addEventListener('mouseover', function(){
+    image7.src='assets/Images/product-001-b.jpg';
+    hoverCart7.style.color='black'
+});
+hoverCart7.addEventListener('mouseout', function(){
+    image7.src='assets/Images/product-001-b.jpg';
+    hoverCart7.style.color='gray'
+
+});
+
+imageContent8.addEventListener('mouseover',function(){
+    hoverContent8.style.visibility='visible'
+});
+imageContent8.addEventListener('mouseout',function(){
+    hoverContent8.style.visibility='hidden'
+});
+hoverEye8.addEventListener('mouseover', function(){
+    image8.src='assets/Images/product-01-d.jpg';
+    hoverEye8.style.color='black';
+});
+hoverEye8.addEventListener('mouseout', function(){
+    image8.src='assets/Images/product-01-d.jpg';
+    hoverEye8.style.color='gray';
+});
+hoverCart8.addEventListener('mouseover', function(){
+    image8.src='assets/Images/product-01-d.jpg';
+    hoverCart8.style.color='black'
+});
+hoverCart8.addEventListener('mouseout', function(){
+    image8.src='assets/Images/product-01-d.jpg';
+    hoverCart8.style.color='gray'
+
+});
+
+
+sale1.addEventListener('mouseover', function(){
+    image5.src='assets/Images/product-01-c.jpg'
+});
+sale1.addEventListener('mouseout', function(){
+    image5.src='assets/Images/product-01-c.jpg'
+});
+
+sale2.addEventListener('mouseover', function(){
+    image6.src='assets/Images/product-01-d.jpg';
+});
+sale2.addEventListener('mouseout', function(){
+    image6.src='assets/Images/product-01-d.jpg';
+});
+
+sale3.addEventListener('mouseover', function(){
+    image7.src='assets/Images/product-001-b.jpg';
+});
+sale3.addEventListener('mouseout', function(){
+    image7.src='assets/Images/product-001-b.jpg';
+});
+
+sale4.addEventListener('mouseover', function(){
+    image8.src='assets/Images/product-01-d.jpg';
+});
+sale4.addEventListener('mouseout', function(){
+    image8.src='assets/Images/product-01-d.jpg';
+});
